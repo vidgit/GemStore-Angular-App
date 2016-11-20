8281ec745deb962f361f66c62375fb98ac1b9829
Vibhor Joshi <vibhorjoshi05@gmail.com>
2016-11-20 20:01:13 +0530

    Completed App

--- js/app.js
+++ js/app.js
@@ -1,151 +1,23 @@
-var gems = [
-    {
-      name: 'Azurite',
-      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-      shine: 8,
-      price: 110.50,
-      rarity: 7,
-      color: '#CCC',
-      faces: 14,
-      canPurchase:true,
-      soldOut:false,
-      images: [
-        "images/gem-02.gif",
-        "images/gem-05.gif",
-        "images/gem-09.gif"
-      ],
-      reviews: [
-      {
-        stars: 5,
-        body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
-      }
-      ]
-    }, 
-    {
-      name: 'Bloodstone',
-      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-      shine: 9,
-      price: 22.90,
-      rarity: 6,
-      color: '#EEE',
-      faces: 12,
-      canPurchase:true,
-      soldOut:false,
-      images: [
-        "images/gem-01.gif",
-        "images/gem-03.gif",
-        "images/gem-04.gif"
-      ],
-      reviews: [
-      {
-        stars: 3,
-        body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 4,
-        body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
-      }
-      ]
-    }, 
-    {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      canPurchase:true,
-      soldOut:false,
-      images: [
-        "images/gem-06.gif",
-        "images/gem-07.gif",
-        "images/gem-10.gif"
-      ],
-      reviews: [
-      {
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
-      }, 
-      {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, 
-      {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }
-      ]
-}];
-var app = angular.module('gemStore',[]);
-app.controller("StoreController",function(){
-    this.products=gems;
+(function() {
+  var app = angular.module('gemStore', ['store-directives']);
 
-    this.resetStore=function(){
-        for(var i=0;i<gems.length;i++){
-            this.products[i].soldOut=false;
-            this.products[i].canPurchase=true;
-            this.products[i].quantity=5;
-        }
-    }
+  app.controller('StoreController', ['$http',function($http){
+    var store = this;
+    store.products =[];
+    $http.get('/store-products.json').success(function(data){
+      store.products = data;
+    });
+    
+  }]);
 
-    this.reduce=function(id){
-        this.products[id-1].quantity=this.products[id-1].quantity-1;
-        if(this.products[id-1].quantity == 0){
-            this.products[id-1].canPurchase=false;
-        }
-    }
-});
+  app.controller("ReviewController", function(){
 
-app.controller('TabController',function(){
-    this.tab=1;
-    this.setTab = function(tabValue){
-      this.tab=tabValue;
-    };
+    this.review = {};
 
-    this.isSet = function(tabValue){
-      return (this.tab===tabValue);
-    };
-  });
-
-app.controller("GalleryController",function(){
-    this.current = 0;
-    this.setCurrent = function(product,image){
-      if(image===null)
-        this.current=0;
-      else
-        this.current=product.images.indexOf(image);
-    };
-});
-
-app.controller("ReviewController",function(){
-      this.review={};
     this.addReview = function(product){
-      this.review.createdOn=Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
-});
 
-app.directive('productDescription',function(){
-    return{
-      restrict: 'E',
-      templateUrl:'js/product-description.html'
-    };
   });
+})();
